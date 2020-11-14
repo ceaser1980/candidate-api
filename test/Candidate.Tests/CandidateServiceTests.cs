35ca2fa892db89d5b252116cde0320fe5b430128
Anthony Roe <anthony.roe70@gmail.com>
2020-11-14 13:54:01 +0000

    Some more tests and running tests in Github actions

--- test/Candidate.Tests/CandidateServiceTests.cs
+++ test/Candidate.Tests/CandidateServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Candidate.Domain.Candidates;
@@ -21,20 +22,22 @@ namespace Candidate.Tests
         }
 
         [Test]
-        public async Task When_RetrieveCandidatesWithSkills_WithSingleSkill()
+        public async Task Given_Retrieving_Candidate_With_Single_Matched_Skill()
         {
             //arrange
-            const string skill = "testString";
+            const string skill = "testSkill";
+            const string candidateName = "Mr Test";
+            Guid candidateId = Guid.NewGuid();
             var skills = new List<string>
             {
                 skill
             };
-            var candidateDtos = new List<CandidateDto>
+            var candidates = new List<CandidateDto>
             {
                 new CandidateDto
                 {
-                    Id = Guid.NewGuid(),
-                    Name = "Mr Test",
+                    Id = candidateId,
+                    Name = candidateName,
                     Skills = new List<SkillDto>
                     {
                         new SkillDto(skill)
@@ -44,14 +47,101 @@ namespace Candidate.Tests
             
             Mock.Get(_candidateDataService)
                 .Setup(x => x.RetrieveAsync(It.IsAny<List<string>>(), It.IsAny<CancellationToken>()))
-                .ReturnsAsync(candidateDtos);
+                .ReturnsAsync(candidates);
 
             //act
             var result = await _candidateService.RetrieveCandidatesWithSkills(skills, CancellationToken.None);
 
             //assert
             Assert.IsTrue(result.IsT0);
-            Assert.IsTrue(result.AsT0.Id != Guid.Empty);
+            Assert.AreEqual(candidateId, result.AsT0.Id);
+            Assert.AreEqual(candidateName, result.AsT0.Name);
+            Assert.AreEqual(skill, result.AsT0.Skills.FirstOrDefault());
+        }
+
+        [Test]
+        public async Task Given_Retrieving_Candidate_With_Multiple_Skills_One_Match()
+        {
+            //arrange
+            const string skill1 = "testSkill0";
+            const string skill2 = "testSkill1";
+            const string candidateName = "Mr Test";
+            Guid candidateId = Guid.NewGuid();
+            var skills = new List<string>
+            {
+                skill1,skill2
+            };
+            var candidates = new List<CandidateDto>
+            {
+                new CandidateDto
+                {
+                    Id = candidateId,
+                    Name = candidateName,
+                    Skills = new List<SkillDto>
+                    {
+                        new SkillDto(skill1)
+                    }
+                }
+            };
+            
+            Mock.Get(_candidateDataService)
+                .Setup(x => x.RetrieveAsync(It.IsAny<List<string>>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(candidates);
+            
+            //act
+            var result = await _candidateService.RetrieveCandidatesWithSkills(skills, CancellationToken.None);
+
+            //assert
+            Assert.IsTrue(result.IsT0);
+            Assert.AreEqual(candidateId, result.AsT0.Id);
+            Assert.AreEqual(candidateName, result.AsT0.Name);
+            Assert.AreEqual(1, result.AsT0.Skills.Length);
+            Assert.AreEqual(skill1, result.AsT0.Skills.FirstOrDefault());
+        }
+        
+        [Test]
+        public async Task Given_Retrieving_Candidate_With_Multiple_Matched_Skills()
+        {
+            //arrange
+            const string skill1 = "testSkill0";
+            const string skill2 = "testSkill1";
+            const string candidateName = "Mr Test";
+            Guid candidateId = Guid.NewGuid();
+            var skills = new List<string>
+            {
+                skill1,skill2
+            };
+            var candidates = new List<CandidateDto>
+            {
+                new CandidateDto
+                {
+                    Id = candidateId,
+                    Name = candidateName,
+                    Skills = new List<SkillDto>
+                    {
+                        new SkillDto(skill1),
+                        new SkillDto(skill2)
+                    }
+                }
+            };
+            
+            Mock.Get(_candidateDataService)
+                .Setup(x => x.RetrieveAsync(It.IsAny<List<string>>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(candidates);
+            
+            //act
+            var result = await _candidateService.RetrieveCandidatesWithSkills(skills, CancellationToken.None);
+
+            //assert
+            Assert.IsTrue(result.IsT0);
+            Assert.AreEqual(candidateId, result.AsT0.Id);
+            Assert.AreEqual(candidateName, result.AsT0.Name);
+            Assert.AreEqual(2, result.AsT0.Skills.Length);
+
+            for (var i = 0; i <= result.AsT0.Skills.Length - 1; i++)
+            {
+                Assert.AreEqual("testSkill" + i, result.AsT0.Skills[i]);
+            }
         }
     }
 }
